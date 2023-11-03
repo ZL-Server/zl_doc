2e523a544f1241d63fb4b3714a702ef9fa0d9d9a
wling <wlingzhenyu@163.com>
2023-11-04 04:08:09 +0800

    Update baseUrl in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
     title: 'ZL Server Wiki',
     tagline: '感谢每一位玩家的到来💡',
     url: 'https://docs.tblstudio.cn',
-    baseUrl: '/',
+    baseUrl: '/zl_doc/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
